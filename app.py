94ae9de4908645c306098acc59c00dd304561806
lahiffbill <lahiffbill@gmail.com>
2024-06-19 19:44:09 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Home Page!"
+
+@app.route('/hello')
+def hello():
+    return "Hello, World!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello, {name}!"
+
+@app.route('/square/<int:number>')
+def square(number):
+    return jsonify({"number": number, "square": number**2})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
